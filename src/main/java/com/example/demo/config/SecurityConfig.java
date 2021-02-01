0d7d95f78778c3438d4429958cbb5f2d5daeaeb7
강현우 <hchc6480@gmail.com>
2021-02-02 00:35:37 +0900

    security 설정

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll(); //모든 경로에 권한없이 접근 가능
+    }
+
+    @Bean
+    public BCryptPasswordEncoder BCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
